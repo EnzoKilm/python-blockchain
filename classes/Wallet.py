87a0746042b0195267c74a1680b56d1d03e8bec8
Enzo Beauchamp <enzo-beauchamp@live.fr>
2021-07-08 12:02:21 +0200

    👛 Wallet update

--- classes/Wallet.py
+++ classes/Wallet.py
@@ -1,20 +1,42 @@
 import uuid
 import os
+import json
 
 
 class Wallet:
     def __init__(self):
         self.unique_id = self.generate_unique_id()
+        self.balance = 0
+        self.history = []
 
     def generate_unique_id(self):
         wallet_uuid = uuid.uuid4()
-        return self.check_unique_id(wallet_uuid)
 
-    def check_unique_id(self, wallet_uuid):
         if os.path.exists(f"./content/wallets/{wallet_uuid}.json"):
             return self.generate_unique_id()
         else:
-            f = open(f"./content/wallets/{wallet_uuid}.json", "w")
-            f.close()
+            self.save()
 
             return wallet_uuid
+
+    def add_balance(self, value):
+        self.balance += value
+
+    def sub_balance(self, value):
+        self.balance -= value
+
+    def send(self):
+        pass
+
+    def save(self):
+        data = {
+            'unique_id': self.unique_id,
+            'balance': self.balance,
+            'history': []
+        }
+
+        for h in self.history:
+            data['history'].append(h)
+
+        with open(f"./content/wallets/{self.unique_id}.json", "w") as file:
+            json.dump(data, file)
