82e492fb71702242e5b77051eda69e95b2b43a2d
Enzo Beauchamp <enzo-beauchamp@live.fr>
2021-07-08 11:44:11 +0200

    🔑 Unique ID added

--- classes/Wallet.py
+++ classes/Wallet.py
@@ -1,4 +1,20 @@
+import uuid
+import os
+
+
 class Wallet:
     def __init__(self):
-        self.unique_id = ""
+        self.unique_id = self.generate_unique_id()
 
+    def generate_unique_id(self):
+        wallet_uuid = uuid.uuid4()
+        return self.check_unique_id(wallet_uuid)
+
+    def check_unique_id(self, wallet_uuid):
+        if os.path.exists(f"./content/wallets/{wallet_uuid}.json"):
+            return self.generate_unique_id()
+        else:
+            f = open(f"./content/wallets/{wallet_uuid}.json", "w")
+            f.close()
+
+            return wallet_uuid
---
+++ main.py
@@ -0,0 +1,5 @@
+from classes.Wallet import Wallet
+
+if __name__ == "__main__":
+    wallet = Wallet()
+    print(wallet.unique_id)
